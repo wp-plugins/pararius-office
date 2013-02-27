d14b00a4f577d9aad6e11ddcb9f2f343a722a989
AnnoMMX <AnnoMMX@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-27 11:03:33 +0000

    Use path provided by WordPress
    git-svn-id: https://plugins.svn.wordpress.org/pararius-office/trunk@674004 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- parariusoffice.php
+++ parariusoffice.php
@@ -15,7 +15,7 @@ require_once 'lib/debug.options.php';
 
 defined('PARARIUSOFFICE_INDEX_FILE') || define('PARARIUSOFFICE_INDEX_FILE', __FILE__);
 defined('PARARIUSOFFICE_PLUGIN_PATH') || define('PARARIUSOFFICE_PLUGIN_PATH', dirname(__FILE__));
-defined('PARARIUSOFFICE_PLUGIN_URL') || define('PARARIUSOFFICE_PLUGIN_URL', WP_PLUGIN_URL . '/parariusoffice');
+defined('PARARIUSOFFICE_PLUGIN_URL') || define('PARARIUSOFFICE_PLUGIN_URL', rtrim(plugin_dir_url(__FILE__), '/'));
 
 // load external libs
 require_once PARARIUSOFFICE_PLUGIN_PATH . '/vendor/load.php';
